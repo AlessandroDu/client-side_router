535a92d9992a255eaaa4aea586580f8d3dff7985
Alessandro Ducci <duccialessandro001@gmail.com>
2025-01-24 22:56:40 +0400

    I added history.pushState() and popstate, with the displayContent() helper func. This allows the router to track browsing history on a single page.

--- router.js
+++ router.js
@@ -7,6 +7,7 @@ const routes = {
 const app = document.getElementById("app");
 
 function navigateTo(path) {
+  history.pushState({ content: routes[path] }, "", `${path}`);
   const content = routes[path] || "<h1>404 - Page Not Found</h1>";
   app.innerHTML = content;
 }
@@ -19,4 +20,19 @@ document.addEventListener("DOMContentLoaded", () => {
       navigateTo(path); // Update the app content
     });
   });
+});
+
+function displayContent(state) {
+  const content = state.content;
+  app.innerHTML = content;
+}
+
+// Handle forward/back buttons
+window.addEventListener("popstate", (event) => {
+  // If a state has been provided, we have a "simulated" page
+  // and we update the current page.
+  if (event.state) {
+    // Simulate the loading of the previous page
+    displayContent(event.state);
+  }
 });
